1e30a726665058c8ea11872f26a1584dcadf978e
wp2catherine <catherinesurya0510@gmail.com>
2023-10-05 20:01:39 +0700

    Penambahan Isi pustaka booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Catherine,
+            Saya tinggal di daerah jakarta pusat.
+            Olahraga yang saya sukai adalah bulutangkis.";
+    }
+}
